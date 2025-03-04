60243bdc5c55f3c885530844a3cc305ab5dab901
Bohdan-dev-web <voloshyn.bhd@gmail.com>
2025-03-04 22:37:10 +0200

    додав материнські плати

--- public/js/mothers.js
+++ public/js/mothers.js
@@ -1,5 +1,20 @@
-const axios = require('axios');
-axios.get('http://192.168.0.103:3000/Mothers')
-    .then(res =>{
-        console.log(res.data);
-    })
+const Mothers = document.querySelector('.Mothers')
+async function getData() {
+    try {
+        const response = await fetch('http://192.168.0.103:3000/Mothers');
+        const data = await response.json();
+        data.forEach(el => {
+            Mothers.innerHTML += 
+            `
+            <div class="Mothers-card">
+                <img class="Mothers-card__img" src="${el.img}" alt="">
+                <p class="Mothers-card__title">${el.brand} ${el.model}</p>
+            </div>
+            ` 
+        });
+    } catch (error) {
+        console.error('Помилка:', error);
+    }
+}
+
+getData()
